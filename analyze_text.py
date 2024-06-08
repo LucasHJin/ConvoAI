fa151c297431692078d96275a823ddfb9fd3c9c3
Lucas Jin <kblazer20@gmail.com>
2024-06-08 08:37:56 -0400

    Started analyzing the text for questions

--- analyze_text.py
+++ analyze_text.py
@@ -1,2 +1,35 @@
 #take text from voice input and parse it for questions
 
+import spacy
+import nltk
+
+
+nlp = spacy.load("en_core_web_sm")
+
+def is_question(text):
+    #tokenize
+    doc = nlp(text.lower())
+    tokens = [(token.text, token.tag_) for token in doc]
+    
+    #grammar rules
+    question_structures = [
+        ('WP', 'VBZ'),  #W's + Verb
+        ('WP', 'VBP'),  #W's + Verb plural
+        ('WRB', 'VBP'),  #Wh-adverb + Verb
+        ('MD', 'PRP'),  #Modal verb + Personal Pronoun
+    ]
+    
+    #check text matching 
+    for structure in question_structures:
+        if all(tag in [tag for (_, tag) in tokens] for tag in structure):
+            return True
+    return False
+
+#text = open("output.txt", "r")
+
+text = "who are you"
+
+if is_question(text):
+    print("The text is a question.")
+else:
+    print("The text is not a question.")
